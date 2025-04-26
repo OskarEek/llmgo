786655d3680e6abc5fca6cadeeadc0706830dc5c
Oskar Eek <oskar.eek99@outlook.com>
2025-04-27 01:34:52 +0200

    Removed unnecessary file

--- llmgo.go
+++
@@ -1,9 +0,0 @@
-package main
-
-//import openAI "github.com/OskarEek/llmgo/LLMClient/providers/openAI"
-
-func main() {
-	//var openAi = openAI.OpenAIClient{APIKey: "test", GptModel: openAI.GptModels.Gpt_41_nano}
-	//openAi.GenerateResponse()
-
-}
