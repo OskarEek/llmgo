6fc1be6f2b929ffb71bf540eef9201046dbbca2a
Oskar Eek <oskar.eek99@outlook.com>
2025-04-26 23:09:30 +0200

    Updated jsonHelper tests

--- jsonHelper_test.go
+++ jsonHelper_test.go
@@ -8,17 +8,39 @@ import (
 	jsonHelper "github.com/OskarEek/llmgo/utilities/jsonHelper"
 )
 
+type ParentTest struct {
+	ParentName string
+	ParentAge  int
+	ParentTo   string
+}
+
 type PersonTest struct {
 	Name     string
 	Age      int
 	Location string
+	Parent   *ParentTest
+}
+
+type FaultyStruct struct {
+	selfReference *FaultyStruct
+}
+
+func TestFaultyStruct(t *testing.T) {
+	var obj FaultyStruct
+	structure, err := jsonHelper.GetJsonStructureFromType(reflect.TypeOf(obj))
+	if err != nil {
+		t.Logf("%s", err)
+	} else {
+		t.Logf("%s", structure)
+		t.FailNow()
+	}
 }
 
 func TestStructJson(t *testing.T) {
 	var obj PersonTest
 	structure, err := jsonHelper.GetJsonStructureFromType(reflect.TypeOf(obj))
 	if err != nil {
-		fmt.Printf("%s", err)
+		t.Logf("%s", err)
 		t.FailNow()
 	}
 	t.Logf("\n%s", structure)
